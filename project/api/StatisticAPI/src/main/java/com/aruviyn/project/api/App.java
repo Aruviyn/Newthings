b2d2b8c68502e74166092fa5db8850c5fabdd153
Fazreil <fazreil@gmail.com>
2021-09-15 15:50:43 +0800

    initial project skeleton

---
+++ project/api/StatisticAPI/src/main/java/com/aruviyn/project/api/App.java
@@ -0,0 +1,13 @@
+package com.aruviyn.project.api;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
