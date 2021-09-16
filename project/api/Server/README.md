fd4285bf60e003bd649ae569e44cd11ac62bdc17
Fazreil <fazreil@gmail.com>
2021-09-17 03:07:41 +0800

    21 - enable swagger for the project

---
+++ project/api/Server/README.md
@@ -0,0 +1,14 @@
+# Server
+Server subproject is the program that is going to host REST API.
+
+## Introduction
+Server subproject is a standalone program. It will not contain any logic except for pieces of codes necessary for running the server
+
+## Swagger
+The server will leverage on Swagger to run the server esp hosting *Swagger UI* to display the REST API exposed.
+
+# Reference:
+https://spring.io/guides/gs/spring-boot/
+https://www.baeldung.com/swagger-2-documentation-for-spring-rest-api
+https://www.tutorialspoint.com/spring_boot/spring_boot_enabling_swagger2.htm
+https://start.spring.io/
---
+++ project/api/Server/src/main/java/com/aruviyn/project/server/controller/SwaggerConfig.java
@@ -0,0 +1,39 @@
+package com.aruviyn.project.server.controller;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import com.google.common.base.Predicate;
+
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.builders.ApiInfoBuilder;
+import springfox.documentation.service.ApiInfo;
+import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
+import static springfox.documentation.builders.PathSelectors.regex;
+import static com.google.common.base.Predicates.or;
+
+@Configuration
+@EnableSwagger2
+public class SwaggerConfig {
+
+	@Bean
+	public Docket postsApi() {
+		return new Docket(DocumentationType.SWAGGER_2).groupName("public-api")
+				.apiInfo(apiInfo()).select().paths(postPaths()).build();
+	}
+
+	private Predicate<String> postPaths() {
+		/* enable this when having more than one paths 
+		return or(regex("/echo/.*"), regex("/conversion/.*"));
+		*/
+		return (regex("/echo/.*"));
+	}
+
+	private ApiInfo apiInfo() {
+		return new ApiInfoBuilder().title("AVP Rest API project")
+				.description("Aruviyn Varmaa Rest-API Project")
+				.licenseUrl("https://www.isc.org/licenses/").version("1.0").build();
+	}
+
+}
