fc099c82f135a3334dd4d97b367a487fb5002ad4
Fazreil <fazreil@gmail.com>
2021-10-04 01:07:08 +0800

    reformat code according to jalopy code format convention

--- project/api/WordUse/src/main/java/com/aruviyn/project/lingo/App.java
+++ project/api/WordUse/src/main/java/com/aruviyn/project/lingo/App.java
@@ -1,20 +1,24 @@
 package com.aruviyn.project.lingo;
 
-import java.io.IOException;
-
 import com.aruviyn.project.lingo.fetcher.impl.api.DictionaryAPIFetcher;
 
-public class App 
+import java.io.IOException;
+
+public class App
 {
     public static void main( String[] args )
     {
-    	DictionaryAPIFetcher dictionaryFetcher = new DictionaryAPIFetcher("https://api.dictionaryapi.dev/api/v2/entries/en/"+args[0]);
-		String responseString = "";
-		try {
-			responseString = dictionaryFetcher.processResponse(dictionaryFetcher.fetch());
-			System.out.println(responseString);
-		}catch(IOException ioEx) {
-			ioEx.printStackTrace();
-		}
+        DictionaryAPIFetcher dictionaryFetcher =
+            new DictionaryAPIFetcher( "https://api.dictionaryapi.dev/api/v2/entries/en/" + args[0] );
+        String responseString = "";
+
+        try
+        {
+            responseString = dictionaryFetcher.processResponse( dictionaryFetcher.fetch(  ) );
+            System.out.println( responseString );
+        } catch ( IOException ioEx )
+        {
+            ioEx.printStackTrace(  );
+        }
     }
 }
--- project/api/WordUse/src/main/java/com/aruviyn/project/lingo/fetcher/Fetcher.java
+++ project/api/WordUse/src/main/java/com/aruviyn/project/lingo/fetcher/Fetcher.java
@@ -2,12 +2,12 @@ package com.aruviyn.project.lingo.fetcher;
 
 import java.io.IOException;
 
-public interface Fetcher {
-	
-	public String fetch() throws IOException;
-	
-	public String processResponse(String originalResponse);
-	
-	public String getSource();
+public interface Fetcher
+{
+    public String fetch(  )
+                 throws IOException;
 
+    public String processResponse( String originalResponse );
+
+    public String getSource(  );
 }
