1f9eaa2998d354d87c35cb99dd349d2c232dd30a
Aruviyn <72690607+Aruviyn@users.noreply.github.com>
2021-10-09 18:15:58 +0800

    Edited App.java comments

--- project/api/StatisticAPI/src/main/java/com/aruviyn/project/api/App.java
+++ project/api/StatisticAPI/src/main/java/com/aruviyn/project/api/App.java
@@ -1,15 +1,15 @@
 package com.aruviyn.project.api;
 
 /**
- * 
+ * This class is to execute the hello world function implemented.
  * @author Aruviyn
  *
  */
 public class App 
 {
 	/**
-	 * 
-	 * @param args
+	 * This function is to print out Hello World. 
+	 * @param args contains the supplied command-line arguments as an array of String objects
 	 */
     public static void main( String[] args )
     {
